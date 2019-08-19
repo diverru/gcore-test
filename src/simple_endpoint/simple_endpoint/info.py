3aec7e90aa0659abee16a1daf730876d06b0f13a
Alexander Kiselyov <diver.ru@gmail.com>
2019-08-19 12:14:36 +0300

    fix notes

--- src/simple_endpoint/simple_endpoint/info.py
+++ src/simple_endpoint/simple_endpoint/info.py
@@ -2,15 +2,13 @@ import pygit2
 
 from datetime import datetime
 from django.conf import settings
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.http import JsonResponse
 
 
 # remember time of the first import=time of the application start
 STARTED_TS = datetime.utcnow()
 
 
-@api_view()
 def get_info(request):
     now = datetime.utcnow()
     repo = pygit2.Repository(settings.REPO_PATH)
@@ -19,7 +17,10 @@ def get_info(request):
     commit_timestamp = last_commit.commit_time
     commit_dt = datetime.fromtimestamp(commit_timestamp)
     # not sure whether there is a better way
-    version_info = repo.describe(max_candidates_tags=1, describe_strategy=pygit2.GIT_DESCRIBE_TAGS)
+    try:
+        version_info = repo.describe(max_candidates_tags=1, describe_strategy=pygit2.GIT_DESCRIBE_TAGS)
+    except pygit2.GitError:
+        version_info = None
     result = {
         "commit": last_commit.hex,
         "commit_date": commit_dt.isoformat() + 'Z',
@@ -28,4 +29,4 @@ def get_info(request):
         "uptime": int((now - STARTED_TS).total_seconds()),
         "started": STARTED_TS.replace(microsecond=0).isoformat() + "Z"
     }
-    return Response(result)
+    return JsonResponse(result)
